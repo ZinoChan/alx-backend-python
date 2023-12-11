5ed527c179c5f4101db284adefd96578710a38f0
ZinoChan <devzinochan@gmail.com>
2023-12-11 23:23:33 +0100

    revert

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,9 +8,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Executes wait_random n times.
-    """
-    wait_times = await asyncio.gather(
-        *tuple(map(lambda _: wait_random(max_delay), range(n)))
-    )
-    return sorted(wait_times)
+    """Execuute multiple coroutines"""
+    coroutines = [wait_random(max_delay) for _ in range(n)]
+    delays: List[float] = await asyncio.gather(*coroutines)
+    return delays
