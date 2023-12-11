75f312e52c70c9b95e3d0394e601932837020022
ZinoChan <devzinochan@gmail.com>
2023-12-11 23:22:10 +0100

    use tuple

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-"""Execute multiple coroutines at the same time with async"""
-from typing import List
+"""Execuute multiple coroutines"""
+
 import asyncio
+from typing import List
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawns wait_random n times with the specified max_delay
-        and returns the list of all the delays (float values)."""
-    futures = [wait_random(max_delay) for _ in range(n)]
-    futures = asyncio.as_completed(futures)
-    delays = [await future for future in futures]
-    return delays
+    """Executes wait_random n times.
+    """
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
