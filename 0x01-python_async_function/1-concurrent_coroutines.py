96b013cea98be57df29dfa6f506c73f24618988f
ZinoChan <devzinochan@gmail.com>
2023-12-11 22:53:44 +0100

    add proper types

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,6 +9,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Execuute multiple coroutines"""
-    coroutines = [wait_random(max_delay) for _ in range(n)]
-    delays = await asyncio.gather(*coroutines)
+    coroutines: List[asyncio.Future[float]] = [
+        wait_random(max_delay) for _ in range(n)]
+    delays: List[float] = await asyncio.gather(*coroutines)
     return delays
