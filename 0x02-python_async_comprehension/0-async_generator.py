72c3bb415cbf65ae768d2ac4fac9412bb34b6b64
ZinoChan <devzinochan@gmail.com>
2023-12-12 20:10:02 +0100

    fix type

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import List
+from typing import Generator
 
 
-async def async_generator() -> List[float]:
+async def async_generator() -> Generator[float, None, None]:
     """Yield random number"""
     for _ in range(10):
         await asyncio.sleep(1)
