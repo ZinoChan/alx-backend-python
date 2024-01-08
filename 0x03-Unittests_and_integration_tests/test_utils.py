ba8a002190fbfdd22b68cdc3543d96f845da3e67
ZinoChan <devzinochan@gmail.com>
2024-01-08 21:24:59 +0100

    fix: add python script

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python3
 import unittest
 from parameterized import parameterized
 from typing import Dict, Tuple, Union
+from unittest.mock import Mock, patch
 from utils import (
-    access_nested_map
+    access_nested_map,
+    get_json,
+    memoize
 )
 
 
@@ -31,3 +35,46 @@ class TestAccessNestedMap(unittest.TestCase):
         """Tests access_nested_map"""
         with self.assertRaises(expected_exception):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Test Json"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload, mock_get):
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+
+    class TestClass:
+        def a_method(self):
+            return 42
+
+        @memoize
+        def a_property(self):
+            return self.a_method()
+
+    @patch('utils.TestMemoize.TestClass.a_method')
+    def test_memoize(self, mock_a_method):
+        """Tests memoize output."""
+        test_instance = self.TestClass()
+
+        result_1 = test_instance.a_property()
+        mock_a_method.assert_called_once()
+
+        mock_a_method.reset_mock()
+
+        result_2 = test_instance.a_property()
+        mock_a_method.assert_not_called()
+
+        self.assertEqual(result_1, result_2)
